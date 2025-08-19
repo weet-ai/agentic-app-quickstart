a3b55844e0742dab4bfcbd44b696730ee15f6071
rafael <rafaelpierre@github.com>
2025-08-19 18:33:27 +0200

    marimo mcp

--- notebooks/01_llm_as_judge.py
+++ notebooks/01_llm_as_judge.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.16"
+__generated_with = "0.14.17"
 app = marimo.App(width="medium")
 
 
@@ -25,19 +25,28 @@ def _(mo):
 @app.cell
 def _():
     from agents import Agent, Runner
-    from agentic_app_quickstart.examples.helpers import get_model
-    from agents import set_tracing_disabled
+    from agentic_app_quickstart.examples.helpers import get_model, get_tracing_provider
+    from dotenv import load_dotenv
 
-    set_tracing_disabled(disabled = True)
-    return Agent, Runner
+    load_dotenv()
+    return Agent, Runner, get_model, get_tracing_provider
 
 
 @app.cell
-def _():
-    from agentic_app_quickstart.examples.helpers import get_trace_provider
+def _(get_tracing_provider):
+    from opentelemetry import trace
+    from opentelemetry.trace import NoOpTracerProvider, ProxyTracerProvider
 
-    trace_provider = get_trace_provider(project_name = "01_llm_as_judge_example")
-    return
+    def activate_tracing():
+        current_provider = trace.get_tracer_provider()
+
+        if isinstance(current_provider, NoOpTracerProvider) or isinstance(current_provider, ProxyTracerProvider):
+            print("Tracing is NOT enabled with Phoenix.")
+            current_provider = get_tracing_provider(project_name = "01_llm_as_judge_example")
+        else:
+            print("Tracing is enabled.")
+            print(f"Trace provider: {current_provider}")
+    return (activate_tracing,)
 
 
 @app.cell
@@ -56,20 +65,23 @@ def _():
 
 
 @app.cell
-def _(Agent, Runner, mcp_fetch_server):
+def _(Agent, Runner, activate_tracing, get_model, mcp_fetch_server):
     async def pipeline(prompt: str):
 
         async with mcp_fetch_server:
 
+            activate_tracing()
+        
             instructions = """
                 "An agent that tries to fetch data based on user's questions.
-                Fetch data from https://weather.com/weather/tenday if needed."
+                Fetch data from https://www.meteoblue.com if needed."
             """
 
             agent = Agent(
                 name = "DataFetcherAgent",
                 instructions = instructions,
-                mcp_servers = [mcp_fetch_server]
+                mcp_servers = [mcp_fetch_server],
+                model=get_model()
             )
 
             result = await Runner.run(starting_agent=agent, input = prompt)
@@ -79,8 +91,6 @@ def _(Agent, Runner, mcp_fetch_server):
 
 @app.cell
 async def _(pipeline):
-
-
     user_input = "What's the weather forecast for Amsterdam, NL?"
     result = await pipeline(prompt = user_input)
     print(result)
